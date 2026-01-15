d709fe38890c3208ba915e23914d91d7aed90e33
Md. Rubayet Islam <77844439+feedofmri@users.noreply.github.com>
2026-01-15 12:34:52 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/torongoo-csh/', // GitHub Pages base path - change if your repo name is different
 })
 
